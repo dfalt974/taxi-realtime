de4b1a2f267b77a35f9f130faf2eba8896fb65c9
Jorge Contreras <jorge.contreras789@gmail.com>
2020-01-04 15:29:40 -0500

    Tested sending and receiving broadcast messages

--- taxi/trips/consumers.py
+++ taxi/trips/consumers.py
@@ -1,11 +1,13 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
-class TaxiConsumer(AsyncJsonWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
 
-    async def disconnect(self, code):
-        await super().disconnect(code)
+class TaxiConsumer(AsyncJsonWebsocketConsumer):    
+    async def connect(self):
+        await self.channel_layer.group_add(
+            group='test',
+            channel=self.channel_name
+        )
+        await self.accept()
 
     async def receive_json(self, content, **kwargs):
         message_type = content.get('type')
@@ -14,3 +16,19 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
                 'type': message_type,
                 'data': content.get('data'),
             })
+
+    async def echo_message(self, message):
+        await self.send_json({
+            'type': message.get('type'),
+            'data': message.get('data'),
+        })
+    
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            group='test',
+            channel=self.channel_name
+        )
+        await super().disconnect(code)
+
+
+    
