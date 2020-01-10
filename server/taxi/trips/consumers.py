66fe0bab3e201e7a7e3175e8224d0d80a14999c7
Jorge Contreras <jorge.contreras789@gmail.com>
2020-01-10 12:50:35 -0500

    Created test and functionality so rider can broadcast to all drivers a request

--- server/taxi/trips/consumers.py
+++ server/taxi/trips/consumers.py
@@ -57,3 +57,18 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
                 channel=self.channel_name
             )
         await super().disconnect(code)
+
+    async def create_trip(self, message):
+        data = message.get('data')
+        trip = await self._create_trip(data)
+        trip_data = NestedTripSerializer(trip).data
+
+        await self.channel_layer.group_send(group='drivers', message={
+            'type': 'echo.message',
+            'data': trip_data
+        })
+
+        await self.send_json({
+            'type': 'echo.message',
+            'data': trip_data
+        })
