0d668e02a818989a3c50f9ba68f969e291f47560
Jorge Contreras <jorge.contreras789@gmail.com>
2020-01-10 12:38:05 -0500

    Added a rider and driver field to the Trip model and tested whether a trip can be created

--- server/taxi/trips/consumers.py
+++ server/taxi/trips/consumers.py
@@ -1,12 +1,20 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
 
+from trips.serializers import NestedTripSerializer, TripSerializer
+
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
 
     @database_sync_to_async
     def _get_user_group(self, user):
         return user.groups.first().name
+
+    @database_sync_to_async
+    def _create_trip(self, data):
+        serializer = TripSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        return serializer.create(serializer.validated_data)
 
     async def connect(self):
         user = self.scope['user']
@@ -24,17 +32,21 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
 
     async def receive_json(self, content, **kwargs):
         message_type = content.get('type')
-        if message_type == 'echo.message':
-            await self.send_json({
-                'type': message_type,
-                'data': content.get('data'),
-            })
+        if message_type == 'create.trip':
+            await self.create_trip(content)
+        elif message_type == 'echo.message':
+            await self.echo_message(content)
+
+    async def create_trip(self, message):
+        data = message.get('data')
+        trip = await self._create_trip(data)
+        await self.send_json({
+            'type': 'echo.message',
+            'data': NestedTripSerializer(trip).data
+        })
 
     async def echo_message(self, message):
-        await self.send_json({
-            'type': message.get('type'),
-            'data': message.get('data'),
-        })
+        await self.send_json(message)
 
     async def disconnect(self, code):
         user = self.scope['user']
